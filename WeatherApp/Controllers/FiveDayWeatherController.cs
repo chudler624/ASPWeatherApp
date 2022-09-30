d4742b56aea2aae7c85c8e6afc8d5eea6f1806d8
Casey Hudler <chudler624@gmail.com>
2022-09-30 13:21:45 -0400

    stubbed out, not working

---
+++ WeatherApp/Controllers/FiveDayWeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.Controllers
+{
+    public class FiveDayWeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WeatherApp/CurrentWeatherApi.cs
@@ -0,0 +1,39 @@
+﻿namespace WeatherApp
+{
+    public class CurrentWeatherApi
+    {
+
+        public CurrentWeatherApi() { }
+
+        public async Task GetTodaysForcastAsync()
+        {
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=38.5&lat=-78.5&units=imperial&lang=en"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "fe96c29976msh7164fa7f072bef1p19efc6jsn0ad83cbe2bb2" },
+		{ "X-RapidAPI-Host", "weatherbit-v1-mashape.p.rapidapi.com" },
+	},
+			};
+			using (var response = client.SendAsync(request).Result)
+			{
+				response.EnsureSuccessStatusCode();
+				var body = response.Content.ReadAsStringAsync().Result;
+				//Console.WriteLine(body);
+
+				
+			}
+
+
+
+		}
+
+
+
+
+
+    }
+}
